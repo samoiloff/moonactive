e805b72e347f2ded6ad401319adbe0a48d8f9ef3
Damir Ismailov <doomer.samoiloff@gmail.com>
2023-03-23 22:12:48 +0100

    touchover is not working on mobile, thus addded "touchmove" listener and handling instead of "touchover". pixi.js version updated

--- src/game/controller/GameCellOverController.ts
+++ src/game/controller/GameCellOverController.ts
@@ -4,31 +4,68 @@ import {FieldCellView} from "../view/FieldCellView";
 import {FieldUtil} from "../utils/FieldUtil";
 import {AnimUtil} from "../utils/AnimUtil";
 import {FieldTileView} from "../view/FieldTileView";
+import * as PIXI from "pixi.js";
 
 export class GameCellOverController extends GameControllerBase {
 
+    protected interactionManager: PIXI.InteractionManager;
+    protected point: PIXI.Point;
+
     initialize() {
+        this.interactionManager = this.view.app.renderer.plugins.interaction;
         this.model.addListener(GameEvent.CELL_OVER_ACTIVATED, this.onCellOverActivated, this);
         this.model.addListener(GameEvent.CELL_OVER_DEACTIVATED, this.onCellOverDeactivated, this);
     }
 
     protected onCellOverActivated(): void {
-        // const interaction = this.view.app.renderer.plugins.interaction;
-        // interaction['mouseOverRenderer'] = true;
-        this.model.addListener(GameEvent.TILE_OVER, this.onTileOver, this);
-        this.model.addListener(GameEvent.TILE_OUT, this.onTileOut, this);
+        this.view.container.on("touchmove", this.onPointerMove, this);
+        // this.model.addListener(GameEvent.TILE_OVER, this.onTileOver, this);
+        // this.model.addListener(GameEvent.TILE_OUT, this.onTileOut, this);
+    }
+
+    protected onPointerMove(event): void {
+        this.point = event.data.global;
+        if (this.point) {
+            const target = this.interactionManager.hitTest(this.point, this.view.fieldView.container);
+            if (target) {
+                const cell: FieldCellView = target['cell'];
+                console.log(cell.x + ":" + cell.y);
+                if (cell) {
+                    this.onTileOver(cell);
+                } else {
+                    this.onTileOut();
+                }
+            } else {
+                this.onTileOut();
+            }
+        }
+        // console.log("onPointerMove" + this.point.x + ":" + this.point.y);
     }
 
     protected onCellOverDeactivated(): void {
-        this.model.removeListener(GameEvent.TILE_OVER, this.onTileOver, this);
-        this.model.removeListener(GameEvent.TILE_OUT, this.onTileOut, this);
+        this.view.shadowView.container.off("touchmove", this.onPointerMove, this);
+        if (this.point) {
+            const target = this.interactionManager.hitTest(this.point, this.view.fieldView.container);
+            if (target) {
+                const cell: FieldCellView = target['cell'];
+                console.log(cell.x + ":" + cell.y);
+                if (cell) {
+                    this.onTileOver(cell);
+                }
+            }
+        }
+        // this.model.removeListener(GameEvent.TILE_OVER, this.onTileOver, this);
+        // this.model.removeListener(GameEvent.TILE_OUT, this.onTileOut, this);
     }
 
     protected onTileOver(cell: FieldCellView): void {
         if (this.model.tilePressed) {
             const index: number = FieldUtil.positionToIndex(cell.x, cell.y);
             const tile: FieldTileView = this.view.fieldView.tiles[index];
-            if (tile !== this.model.tilePressed && !tile.merged) {
+            if (tile !== this.model.tilePressed && !tile.merged && tile !== this.model.tileMergeTo) {
+                if (this.model.tileMergeTo) {
+                    this.onTileOut();
+                }
                 this.model.tileMergeTo = tile
                 if (this.model.tileMergeTo !== this.model.tilePressed) {
                     if (this.model.tilePressed.tileId === this.model.tileMergeTo.tileId) {
